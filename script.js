ac4317d6d32aa6672a151bda1b9d2adf87db1689
NagiMuseibli <nagimuseibli02@gmail.com>
2024-01-01 22:47:24 +0400

    Creating classes name

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const cartItems = document.querySelectorAll(".cart-items");
 const cartTotal = document.querySelectorAll(".total-value");
 const cartContent = document.querySelectorAll(".cart-list");
 const productsDom = document.querySelectorAll(".products-dom");
+
+
+class Products {
+
+}
+
+class UI {
+
+}
+
+class Storage {
+
+}
