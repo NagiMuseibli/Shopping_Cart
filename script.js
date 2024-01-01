2a2dbb827d571a45ad2e69633f720609cf6d56db
NagiMuseibli <nagimuseibli02@gmail.com>
2024-01-01 22:36:57 +0400

    Variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const cartBtn = document.querySelector(".cart-btn");
+const clearBtn = document.querySelector(".btn-clear");
+const cartItems = document.querySelectorAll(".cart-items");
+const cartTotal = document.querySelectorAll(".total-value");
+const cartContent = document.querySelectorAll(".cart-list");
+const productsDom = document.querySelectorAll(".products-dom");
